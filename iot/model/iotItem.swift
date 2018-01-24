8184679a1fbdd8c336cc288b549ec1dcfc9e25b1
robert@Mac <robert.jonas@gmx.at>
2018-01-24 19:10:55 +0100

    iotItem basic setup

---
+++ iot/model/iotItem.swift
@@ -0,0 +1,16 @@
+//
+//  iotItem.swift
+//  iot
+//
+//  Created by Robert Jonas on 24.01.18.
+//  Copyright © 2018 jr-soft. All rights reserved.
+//
+
+import Foundation
+
+
+class iotItem {
+    
+
+    
+}
